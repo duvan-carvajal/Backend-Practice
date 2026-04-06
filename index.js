87acf66c6e2554ec02f246c026109ec4abedcf7e
duvan-carvajal <duvan.carvajalp@uniagustiniana.edu.co>
2026-04-06 16:29:32 -0500

    crud test

--- index.js
+++ index.js
@@ -2,7 +2,49 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8080;
+app.use(express.json());
+let notes = [];
 
+app.post('/notes', (req, res) =>{
+  const newNote = {
+    text: req.body.text,
+    id: notes.length + 1
+  };
+  notes.push(newNote);
+  res.json({
+    message: "noteAdded",
+    notes: notes
+  });
+});
+
+app.delete('/notes', (req, res) => {
+  notes = [];
+  res.json({ message: 'All notes deleted' });
+});
+
+app.delete('/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+  res.json({ message: `Note ${id} deleted`, notes });
+});
+
+app.put('/notes/:id', (req, res) =>{
+  const id = parseInt(req.params.id);
+  const note = notes.find( n => n.id === id);
+  if (!note) {
+  return res.status(404).json({ message: "Note not found" });
+}
+
+note.text = req.body.text;
+if (!req.body.text) {
+  return res.status(400).json({ message: "Text is required" });
+}
+res.json({message: "Note Updated", note});
+})
+
+app.get('/notes', (req, res) => {
+  res.json(notes);
+})
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 })
